78738592fc3fa3ea59c666ee0c43c872ff54ea4b
ib <ib@WINIB-7F7LP9FGE>
2019-07-10 15:19:28 +0200

    create model exemplaire

--- projet_biblio/src/fr/ib/model/Exemplaire.java
+++ projet_biblio/src/fr/ib/model/Exemplaire.java
@@ -2,6 +2,44 @@ package fr.ib.model;
 
 public class Exemplaire {
 	
-	//test
+	//attributs
+	private int idEx;
+	private String etat;
+	private int idLivre;
+	
+	//constructor
+	public Exemplaire(int idEx, String etat, int idLivre) {
+		super();
+		this.idEx = idEx;
+		this.etat = etat;
+		this.idLivre = idLivre;
+	}
+
+	
+	//getters and setters
+	public int getIdEx() {
+		return idEx;
+	}
+
+	public void setIdEx(int idEx) {
+		this.idEx = idEx;
+	}
+
+	public String getEtat() {
+		return etat;
+	}
+
+	public void setEtat(String etat) {
+		this.etat = etat;
+	}
+
+	public int getIdLivre() {
+		return idLivre;
+	}
+
+	public void setIdLivre(int idLivre) {
+		this.idLivre = idLivre;
+	}
+	
 
 }
