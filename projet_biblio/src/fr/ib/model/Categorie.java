550568b97ea72e9aa6440a98bf62dbcab2c863e0
ib <ib@WINIB-7F3M0TDKM>
2019-07-10 15:14:57 +0200

    ced : commit categorie model2

--- projet_biblio/src/fr/ib/model/Categorie.java
+++ projet_biblio/src/fr/ib/model/Categorie.java
@@ -2,6 +2,45 @@ package fr.ib.model;
 
 public class Categorie {
 	
-	//test
+	//attributs
+		private int idCat;
+		private String nom;
+		
+		//constructeurs
+		public Categorie() {
+			super();
+		}
+		public Categorie(String nom) {
+			super();
+			this.nom = nom;
+		}
+		public Categorie(int idCat, String nom) {
+			super();
+			this.idCat = idCat;
+			this.nom = nom;
+		}
+		
+		
+		//getters et setters
+		public int getIdCat() {
+			return idCat;
+		}
+		public void setIdCat(int idCat) {
+			this.idCat = idCat;
+		}
+		public String getNom() {
+			return nom;
+		}
+		public void setNom(String nom) {
+			this.nom = nom;
+		}
+		
+		
+		///toString
+
+		@Override
+		public String toString() {
+			return "Categorie [idCat=" + idCat + ", nom=" + nom + "]";
+		}
 
 }
