0ea82d7617ca6ecaba4f3ed223b47a78cd6af13c
ib <ib@WINIB-7F3M0TDKM>
2019-07-10 14:38:57 +0200

    ced: model auteur editeur

--- projet_biblio/src/fr/ib/model/Auteur.java
+++ projet_biblio/src/fr/ib/model/Auteur.java
@@ -2,6 +2,51 @@ package fr.ib.model;
 
 public class Auteur {
 	
-	//test
+//attributs	
+private int idAut;
+private String nom;
+
+//constructeurs
+public Auteur() {
+	super();
+}
+
+public Auteur(String nom) {
+	super();
+	this.nom = nom;
+}
+
+
+public Auteur(int idAut, String nom) {
+	super();
+	this.idAut = idAut;
+	this.nom = nom;
+}
+
+
+//getters et setters
+public int getIdAut() {
+	return idAut;
+}
+
+public void setIdAut(int idAut) {
+	this.idAut = idAut;
+}
+
+public String getNom() {
+	return nom;
+}
+
+public void setNom(String nom) {
+	this.nom = nom;
+}
+
+
+//toString
+@Override
+public String toString() {
+	return "Auteur [idAut=" + idAut + ", nom=" + nom + "]";
+}
+
 
 }
